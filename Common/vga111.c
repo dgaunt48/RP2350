a28af7b17dbed637e848801f21c5b4de557f7821
Dave Gaunt <dgaunt48@gmail.com>
2026-04-03 03:27:08 +1100

    - Added the common VGA functions I use all the time.

---
+++ Common/vga111.c
@@ -0,0 +1,236 @@
+//------------------------------------------------------------------------------------------------
+//----                                                                                        ----
+//------------------------------------------------------------------------------------------------
+//----                                                                                        ----
+//------------------------------------------------------------------------------------------------
+
+#include <stdio.h>
+#include "types.h"
+#include "pico/stdlib.h"
+
+#include "vga111.h"
+
+#include "hardware/pio.h"
+#include "hardware/dma.h"
+
+#include "hsync.pio.h"
+#include "vsync.pio.h"
+#include "rgb.pio.h"
+
+#include "VicChars.h"
+
+const u8 g_aHexTable[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+u8 volatile aVGAScreenBuffer[(VGA_RESOLUTION_X * VGA_RESOLUTION_Y) >> 1];
+volatile u8* address_pointer = aVGAScreenBuffer;
+
+//------------------------------------------------------------------------------------------------
+//----                                                                                        ----
+//------------------------------------------------------------------------------------------------
+void vga_Init(const u32 uPinRGB, const u32 uPinHSync, const u32 uPinVSync)
+{
+	// Choose which PIO instance to use (there are two instances, each with 4 state machines)
+	PIO pio = pio0;
+	const uint hsync_offset = pio_add_program(pio, &hsync_program);
+	const uint vsync_offset = pio_add_program(pio, &vsync_program);
+	const uint rgb_offset = pio_add_program(pio, &rgb_program);
+
+	// Manually select a few state machines from pio instance pio0.
+	uint hsync_sm = 0;
+	uint vsync_sm = 1;
+	uint rgb_sm = 2;
+	hsync_program_init(pio, hsync_sm, hsync_offset, uPinHSync);
+	vsync_program_init(pio, vsync_sm, vsync_offset, uPinVSync);
+	rgb_program_init(pio, rgb_sm, rgb_offset, uPinRGB);
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+	// ============================== PIO DMA Channels =================================================
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	// DMA channels - 0 sends color data, 1 reconfigures and restarts 0
+	int rgb_chan_0 = 2;		// Use Channels 2 & 3 As 1 & 2 Are Used By The SD_Card Library.
+	int rgb_chan_1 = 3;
+
+	// Channel Zero (sends color data to PIO VGA machine)
+	dma_channel_config c0 = dma_channel_get_default_config(rgb_chan_0);  	// default configs
+	channel_config_set_transfer_data_size(&c0, DMA_SIZE_8);              	// 8-bit txfers
+	channel_config_set_read_increment(&c0, true);                        	// yes read incrementing
+	channel_config_set_write_increment(&c0, false);                      	// no write incrementing
+	channel_config_set_dreq(&c0, DREQ_PIO0_TX2) ;                        	// DREQ_PIO0_TX2 pacing (FIFO)
+	channel_config_set_chain_to(&c0, rgb_chan_1);                        	// chain to other channel
+
+	dma_channel_configure
+	(
+		rgb_chan_0,                                                        	// Channel to be configured
+		&c0,                                                               	// The configuration we just created
+		&pio->txf[rgb_sm],                                                 	// write address (RGB PIO TX FIFO)
+		&aVGAScreenBuffer,                                                 	// The initial read address (pixel color array)
+		(VGA_RESOLUTION_X * VGA_RESOLUTION_Y) >> 1,                        	// Number of transfers; in this case each is 1 byte.
+		false                                                              	// Don't start immediately.
+	);
+
+	// Channel One (reconfigures the first channel)
+	dma_channel_config c1 = dma_channel_get_default_config(rgb_chan_1);  	// default configs
+	channel_config_set_transfer_data_size(&c1, DMA_SIZE_32);             	// 32-bit txfers
+	channel_config_set_read_increment(&c1, false);                       	// no read incrementing
+	channel_config_set_write_increment(&c1, false);                      	// no write incrementing
+	channel_config_set_chain_to(&c1, rgb_chan_0);                        	// chain to other channel
+
+	dma_channel_configure
+	(
+		rgb_chan_1,                         	// Channel to be configured
+		&c1,                                	// The configuration we just created
+		&dma_hw->ch[rgb_chan_0].read_addr,  	// Write address (channel 0 read address)
+		&address_pointer,                   	// Read address (POINTER TO AN ADDRESS)
+		1,                                 	 	// Number of transfers, in this case each is 4 byte
+		false                               	// Don't start immediately.
+	);
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	// Initialize PIO state machine counters. This passes the information to the state machines
+	// that they retrieve in the first 'pull' instructions, before the .wrap_target directive
+	// in the assembly. Each uses these values to initialize some counting registers.
+	#define H_ACTIVE   655    // (active + frontporch - 1) - one cycle delay for mov
+	#define V_ACTIVE   479    // (active - 1)
+	#define RGB_ACTIVE 319    // (horizontal active)/2 - 1
+	// #define RGB_ACTIVE 639 // change to this if 1 pixel/byte
+	pio_sm_put_blocking(pio, hsync_sm, H_ACTIVE);
+	pio_sm_put_blocking(pio, vsync_sm, V_ACTIVE);
+	pio_sm_put_blocking(pio, rgb_sm, RGB_ACTIVE);
+
+	// Start the two pio machine IN SYNC
+	// Note that the RGB state machine is running at full speed,
+	// so synchronization doesn't matter for that one. But, we'll
+	// start them all simultaneously anyway.
+	pio_enable_sm_mask_in_sync(pio, ((1u << hsync_sm) | (1u << vsync_sm) | (1u << rgb_sm)));
+
+	// Start DMA channel 0. Once started, the contents of the pixel color array
+	// will be continously DMA's to the PIO machines that are driving the screen.
+	// To change the contents of the screen, we need only change the contents
+	// of that array.
+	dma_start_channel_mask((1u << rgb_chan_0)) ;
+}
+
+//------------------------------------------------------------------------------------------------
+//----                                                                                        ----
+//------------------------------------------------------------------------------------------------
+void vga_FilledRect(u32 uPositionX, u32 uPositionY, u32 uWidth, u32 uHeight, u32 uColour)
+{
+	if (uPositionX + uWidth >= VGA_RESOLUTION_X)
+		uWidth = VGA_RESOLUTION_X - uPositionX;
+
+	if (uPositionY + uHeight >= VGA_RESOLUTION_Y)
+		uHeight = VGA_RESOLUTION_Y - uPositionY;
+
+	if ((uWidth > 0) && (uHeight > 0))
+	{
+		u32 uPixelOffset = ((uPositionY * VGA_RESOLUTION_X) + uPositionX) >> 1;
+
+		if (uPositionX & 1)
+		{
+			u32 uOffset = uPixelOffset++;
+			--uWidth;
+
+			for(u32 y=0; y<uHeight; ++y)
+			{
+				aVGAScreenBuffer[uOffset] = (aVGAScreenBuffer[uOffset] & 0b11000111) | (uColour << 3);
+				uOffset += VGA_RESOLUTION_X >> 1;
+			}
+		}
+
+		while (uWidth > 1)
+		{
+			u32 uOffset = uPixelOffset++;
+			uWidth -= 2;
+
+			for(u32 y=0; y<uHeight; ++y)
+			{
+				aVGAScreenBuffer[uOffset] = (uColour << 3) | uColour;
+				uOffset += VGA_RESOLUTION_X >> 1;
+			}
+		}
+
+		if (1 == uWidth)
+		{
+			for(u32 y=0; y<uHeight; ++y)
+			{
+				aVGAScreenBuffer[uPixelOffset] = (aVGAScreenBuffer[uPixelOffset] & 0b11111000) | uColour;
+				uPixelOffset += VGA_RESOLUTION_X >> 1;
+			}
+		}
+	}
+}
+
+//------------------------------------------------------------------------------------------------
+//---- Caution - No Clipping!                                                                 ----
+//------------------------------------------------------------------------------------------------
+void vga_CopyRect(const u8* pSource, const u32 uXPos, const u32 uYPos, const u32 uWidth, const u32 uHeight, const u8* pColourTable)
+{
+	u32 uBufferOffset = ((uYPos * VGA_RESOLUTION_X) + uXPos) >> 1;
+	u32 uCopyWidth = uWidth >> 1;
+
+	for (u32 y=0; y<uHeight; ++y)
+	{
+		for (u32 x=0; x<uCopyWidth; ++x)
+		{
+			u8 uPixelPair =  pColourTable[pSource[((uBufferOffset + x) << 1)    ]];
+			uPixelPair   |= (pColourTable[pSource[((uBufferOffset + x) << 1) + 1]] << 3);
+			aVGAScreenBuffer[uBufferOffset + x] = uPixelPair;
+		}
+
+		uBufferOffset += (VGA_RESOLUTION_X >> 1);
+	}
+}
+
+//------------------------------------------------------------------------------------------------
+//----                                                                                        ----
+//------------------------------------------------------------------------------------------------
+void vga_DrawPetsciiChar(const u32 uXPos, const u32 uYPos, const u8 uChar, const u8 uColour)
+{
+	for (u32 uLine=0; uLine<8; ++uLine)
+	{
+		u32 uPixelOffset = ((((uYPos + uLine) * VGA_RESOLUTION_X ) + uXPos) >> 1) + 3;
+		u32 uCharLine = VicChars901460_03[2048 + (uChar << 3) + uLine];
+
+		for (u32 x=0; x<4; ++x)
+		{
+			u8 uPixelPair = 0;
+
+			if (uCharLine & 2)
+				uPixelPair = uColour;
+
+			if (uCharLine & 1)
+				uPixelPair |= (uColour << 3);
+
+			aVGAScreenBuffer[uPixelOffset--] = uPixelPair;
+			uCharLine >>= 2;
+		}
+	}
+}
+
+//------------------------------------------------------------------------------------------------
+//----                                                                                        ----
+//------------------------------------------------------------------------------------------------
+void vga_DrawString(u32 uCharX, u32 uCharY, const char* pszString, const u8 uColour)
+{
+	while (*pszString)
+	{
+		if (uCharX >= (TERMINAL_CHARS_WIDE-1))
+		{
+			uCharX = 1;
+			++uCharY;
+		}
+
+		if (uCharY >= (TERMINAL_CHARS_HIGH-1))
+			return;
+
+		u8 c = *pszString++;
+
+		if (c >= '`')
+			c -= '`';
+
+		vga_DrawPetsciiChar(uCharX << 3, uCharY << 3, c, uColour);
+		++uCharX;
+	}
+}
---
+++ Common/vga111.h
@@ -0,0 +1,31 @@
+//------------------------------------------------------------------------------------------------
+//----                                                                                        ----
+//------------------------------------------------------------------------------------------------
+
+#ifndef __vga111_h_included
+#define __vga111_h_included
+
+#include "types.h"
+#include "hsync.pio.h"
+#include "vsync.pio.h"
+#include "rgb.pio.h"
+
+#define VGA_RESOLUTION_X    	(640)
+#define VGA_RESOLUTION_Y  		(480)
+#define TERMINAL_CHARS_WIDE		(VGA_RESOLUTION_X >> 3)
+#define TERMINAL_CHARS_HIGH		(VGA_RESOLUTION_Y >> 3)
+
+extern const u8 g_aHexTable[];
+
+void vga_Init(const u32 uPinRGB, const u32 uPinHSync, const u32 uPinVSync);
+void vga_FilledRect(u32 uPositionX, u32 uPositionY, u32 uWidth, u32 uHeight, u32 uColour);
+void vga_CopyRect(const u8* pSource, const u32 uXPos, const u32 uYPos, const u32 uWidth, const u32 uHeight, const u8* pColourTable);
+void vga_DrawPetsciiChar(const u32 uXPos, const u32 uYPos, const u8 uChar, const u8 uColour);
+void vga_DrawString(u32 uCharX, u32 uCharY, const char* pszString, const u8 uColour);
+
+static inline u16 byteToHex(const u8 uByte)
+{
+	return (g_aHexTable[(uByte >> 4) & 15] << 8) | g_aHexTable[uByte & 15];
+}
+
+#endif // __vga111_h_included
